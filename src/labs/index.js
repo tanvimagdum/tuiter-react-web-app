142d3b254524c7352eb1a7da6889bc87407b4737
tanvimagdum <magdum.t@northeastern.edu>
2023-05-30 19:11:26 -0400

    new a4 commit

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
 
 
 function Labs() {
     return(
        <div>
          <Nav/>
-         <Assignment3/>
+         <Routes>
+            <Route path="a3" element={<Assignment3/>}/>
+            <Route path="a4" element={<Assignment4/>}/>
+         </Routes>
        </div>
     );
  }
