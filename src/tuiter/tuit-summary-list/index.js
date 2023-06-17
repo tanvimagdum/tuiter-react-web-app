b1d0d294dd6bbf2508f9472f66ed4adb64e5a798
tanvimagdum <magdum.t@northeastern.edu>
2023-06-17 14:06:21 -0400

    touch-ups

--- src/tuiter/tuit-summary-list/index.js
+++ src/tuiter/tuit-summary-list/index.js
@@ -1,11 +1,21 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import TuitSummaryItem from "./tuit-summary-item";
+import {findTuitsThunk} from "../services/tuits-thunks";
 
 const TuitSummaryList = () => {
-  const { tuits } = useSelector(state => state.tuits)
+  const { tuits, loading } = useSelector(state => state.tuits)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk())
+  }, []); 
   return(
     <ul className="list-group">
+      { loading &&
+       <li className="list-group-item">
+         Loading...
+       </li>
+      }
       {
         tuits.map(tuit =>
           <TuitSummaryItem
--- src/tuiter/whats-happening.js
+++ src/tuiter/whats-happening.js
@@ -7,47 +7,78 @@ import { BsEmojiSmile } from "react-icons/bs";
 import { MdFormatListBulleted } from "react-icons/md";
 import { TbCalendarStats } from "react-icons/tb";
 import {createTuitThunk} from "./services/tuits-thunks";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 
 const WhatsHappening = () => {
     let [whatsHappening, setWhatsHappening] = useState('');
+    const { currentUser } = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const tuitClickHandler = () => {
         const newTuit = {
-            tuit: whatsHappening
+            tuit: whatsHappening,
+            username: currentUser.username,
+            handle: currentUser.handle,
+            image: currentUser.image
         }
         dispatch(createTuitThunk(newTuit));
         setWhatsHappening("");
     }
     return (
     <div className="row">
-        <div className="col-auto">
-        <img src="/images/kate.jpg" width={60}/>
-        </div>
-        <div className="col-10">
-        <textarea value={whatsHappening} placeholder="What's happening?"
-                className="form-control border-0"
-                onChange={(event) => setWhatsHappening(event.target.value)}>
-        </textarea>
-        <div>
-            <button className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
-                    onClick={tuitClickHandler}>
-            Tuit
-            </button>
-            <div className="text-primary fs-2">
-            <AiOutlinePicture className="me-3"/>
-            <HiOutlineGif className="me-3"/>
-            <MdFormatListBulleted className="me-3"/>
-            <BsEmojiSmile className="me-3"/>
-            <TbCalendarStats className="me-3"/>
-            <HiOutlineLocationMarker className="me-3"/>
-            <BiBold className="me-3"/>
-            <BiItalic className="me-3"/>
+        {currentUser ? 
+        <>
+            <div className="col-auto">
+                <img className="rounded-circle" src={`/images/${currentUser.image}`} width={60}/>
             </div>
-        </div>
-        </div>
-        <div className="col-12"><hr/></div>
+            <div className="col-10">
+                <textarea value={whatsHappening} placeholder="What's happening?"
+                        className="form-control border-0"
+                        onChange={(event) => setWhatsHappening(event.target.value)}>
+                </textarea>
+                <div>
+                    <button className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
+                            onClick={tuitClickHandler}>
+                        Tuit
+                    </button>
+                    <div className="text-primary fs-2">
+                        <AiOutlinePicture className="me-3"/>
+                        <HiOutlineGif className="me-3"/>
+                        <MdFormatListBulleted className="me-3"/>
+                        <BsEmojiSmile className="me-3"/>
+                        <TbCalendarStats className="me-3"/>
+                        <HiOutlineLocationMarker className="me-3"/>
+                        <BiBold className="me-3"/>
+                        <BiItalic className="me-3"/>
+                    </div>
+                </div> 
+                <div className="col-12"><hr/></div>
+            </div>
+        </> :
+        <>
+            <div className="col-auto">
+                <img className="rounded-circle" src="/images/kate.jpg" width={60}/>
+            </div>
+            <div className="col-10">
+                <textarea placeholder="What's happening?"
+                        className="form-control border-0">
+                </textarea>
+                <div>
+                    <p className="float-end mt-2 ps-3 pe-3 fw-bold">Login to post a tuit.</p>
+                    <div className="text-primary fs-2">
+                        <AiOutlinePicture className="me-3"/>
+                        <HiOutlineGif className="me-3"/>
+                        <MdFormatListBulleted className="me-3"/>
+                        <BsEmojiSmile className="me-3"/>
+                        <TbCalendarStats className="me-3"/>
+                        <HiOutlineLocationMarker className="me-3"/>
+                        <BiBold className="me-3"/>
+                        <BiItalic className="me-3"/>
+                    </div>
+                </div>
+                <div className="col-12"><hr/></div>
+            </div>
+        </>}
     </div>
     );
 }
